916ad299aaf1d740ab428ed8bedd51d5b846dab3
michellejanosi <michelle.janosi@gmail.com>
2022-10-10 06:07:56 -0400

    Add function to draw 2 cards

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let deckId;
 
 const newDeckButton = document.getElementById("new-deck");
+const drawCardsButton = document.getElementById("draw-card");
+
 const getNewDeck = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((response) => response.json())
@@ -11,4 +13,13 @@ const getNewDeck = () => {
     });
 }
 
+const drawTwoCards = () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
 newDeckButton.addEventListener("click", getNewDeck);
+drawCardsButton.addEventListener("click", drawTwoCards);
