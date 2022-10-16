ac4b8017f6b41098bd7a772aa1888299f3dc96e4
michellejanosi <michelle.janosi@gmail.com>
2022-10-16 10:31:10 -0400

    Add remaining cards and scores

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const newDeckButton = document.getElementById("new-deck");
 const drawCardsButton = document.getElementById("draw-card");
 const headerText = document.getElementById("header-text");
 const remainingCards = document.getElementById("remaining-cards");
+const computerScoreText = document.getElementById("computer-score");
+const playerScoreText = document.getElementById("player-score");
+let computerScore = 0;
+let playerScore = 0;
+
 
 const getNewDeck = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((response) => response.json())
     .then((data) => {
-      // console.log(data);
       deckId = data.deck_id;
+      remainingCards.textContent = `Remaining cards: 52`;
     });
 }
 
@@ -20,8 +25,6 @@ const drawTwoCards = () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((response) => response.json())
     .then((data) => {
-      // console.log(data.cards);
-      console.log(data.remaining)
       cardsDiv.children[0].innerHTML = `<img src="${data.cards[0].image}" class="card-image" alt="card">`;
       cardsDiv.children[1].innerHTML = `<img src="${data.cards[1].image}" class="card-image" alt="card">`;
 
@@ -35,13 +38,13 @@ function determineCardWinner(card1, card2) {
   const valueOptions = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"];
   const card1ValueIndex = valueOptions.indexOf(card1.value);
   const card2ValueIndex = valueOptions.indexOf(card2.value);
-  console.log("card 1:", card1ValueIndex);
-  console.log("card 2:", card2ValueIndex);
 
   if (card1ValueIndex > card2ValueIndex) {
     headerText.textContent = "Computer Wins!";
+    computerScoreText.innerHTML = `Computer Score: ${computerScore += 1}`;
   } else if (card1ValueIndex < card2ValueIndex) {
     headerText.textContent = "You Win!";
+    playerScoreText.innerHTML = `Your Score: ${playerScore += 1}`;
   } else {
     headerText.textContent = "War!";
   }
